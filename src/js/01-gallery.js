84bf9d82768bd8bcd17d1255e3801aa45a68c5d2
OksanaShyian <108744464+OksanaShyian@users.noreply.github.com>
2022-12-02 22:06:58 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+const galleryEl = document.querySelector('.gallery');
+
+const onLargeImage = e => {
+  const arrayEl = galleryItems
+    .map(
+      item => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join('');
+
+  galleryEl.innerHTML = arrayEl;
+};
+
+onLargeImage();
+// galleryEl.addEventListener("click", onLargeImage);
+
+// function onLargeImage(e) {
+//   e.preventDefault();
+//   if (e.target === e.currentTarget) return;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
